8d82edef068910115030da8fe140728bc359172f
lacram <chemin9898@gmail.com>
2025-12-27 16:25:45 +0900

    Add root health endpoint

---
+++ backend/src/main/java/com/goalapp/controller/RootController.java
@@ -0,0 +1,18 @@
+package com.goalapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Root endpoint for load balancer and uptime checks.
+ */
+@RestController
+public class RootController {
+
+    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.HEAD})
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("OK");
+    }
+}
